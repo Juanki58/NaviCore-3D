bff462a101609865a3f0209be83350235524f2c8
Juanki58 <jcpmesgemini@gmail.com>
2026-07-11 17:33:38 +0200

    FASE G: Implementado Mecanismo de Consistencia Cinemática en fusion.cpp para detección y aislamiento de fallos en la odometría

--- src/core/fusion.hpp
+++ src/core/fusion.hpp
@@ -7,6 +7,15 @@
 #include "NavState.h"
 #include "sensor_types.hpp"
 
+typedef uint8_t NavQuality;
+
+#define NAVICORE_QUALITY_NONE         0x00U
+#define NAVICORE_QUALITY_ODOM_FAULT   0x01U
+
+#ifndef NAVICORE_ODOM_FAULT_THRESHOLD
+#define NAVICORE_ODOM_FAULT_THRESHOLD 5.0f
+#endif
+
 typedef struct {
     NavState state;
     float imu_weight;
@@ -15,6 +24,9 @@ typedef struct {
     float last_pressure_pa;
     uint32_t last_pressure_timestamp_ms;
     bool pressure_sample_valid;
+    NavQuality quality;
+    float imu_predicted_speed_mps;
+    bool imu_speed_prediction_valid;
 } DeadReckoningFilter;
 
 void dead_reckoning_init(DeadReckoningFilter *filter, Vector3D initial_position, NavDomain domain);
@@ -22,5 +34,8 @@ bool dead_reckoning_update_imu(DeadReckoningFilter *filter, const ImuSample *imu
 bool dead_reckoning_update_gps(DeadReckoningFilter *filter, const GpsSample *gps);
 bool dead_reckoning_update_pressure(DeadReckoningFilter *filter, const PressureSample *pressure, float surface_pressure_pa);
 bool dead_reckoning_update_wheel_odometry(DeadReckoningFilter *filter, float speed_mps, bool reverse, uint32_t timestamp_ms);
+
+NavQuality dead_reckoning_get_quality(const DeadReckoningFilter *filter);
+bool dead_reckoning_has_odom_fault(const DeadReckoningFilter *filter);
 
 #endif /* NAVICORE_FUSION_HPP */
